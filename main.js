acc0ab2d797544255e283b39c01521f619362704
kimin3004 <kimin3004@gmail.com>
2021-05-30 19:49:03 +0900

    Finish Project

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(123)
+let str = `
+010-1234-5678.
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+동해물과_백두산이_마르고_닳도록
+`
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
